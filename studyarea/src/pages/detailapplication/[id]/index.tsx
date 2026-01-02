3eaf1e32d81abb0e706edf1ea9e1148a53dfb136
Mukund madhav <mukundm054@gmail.com>
2026-01-02 15:03:15 +0530

    Update API endpoint to production URL

--- studyarea/src/pages/detailapplication/[id]/index.tsx
+++ studyarea/src/pages/detailapplication/[id]/index.tsx
@@ -14,7 +14,7 @@ const index = () => {
       try {
         setloading(true);
         const res = await axios.get(
-          `http://localhost:5000/api/application/${id}`
+          `https://study-area-ko6n.onrender.com/api/application/${id}`
         );
 
         setdata(res.data);
