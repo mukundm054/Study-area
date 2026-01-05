017453b402e245c32b53c1067aca61aa0f81c8e5
mukundm054 <mukundm054@gmail.com>
2026-01-05 15:57:52 +0530

    Update index.js

--- Backend/Routes/index.js
+++ Backend/Routes/index.js
@@ -11,6 +11,6 @@ router.use("/admin", admin);
 router.use("/interships", intern);
 router.use("/jobs", job);
 router.use("/application",application)
-router.use("/user,", User);
+router.use("/user", User);
 router.use("/post",Post)
 module.exports = router;
