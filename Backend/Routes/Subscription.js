f487b2cb83b93a4909a7ccb308adb37b0a08e128
mukundm054 <mukundm054@gmail.com>
2026-01-27 13:55:23 +0530

    adding payment verification
    added payment verification router

--- Backend/Routes/Subscription.js
+++ Backend/Routes/Subscription.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const razorpay = require("../config/razorpay");
 const SUBSCRIPTION_PLAN = require("../config/subscriptionPlan");
+const crypto = require("crypto");
+const { error } = require("console");
+const User = require("../model/User");
 
 router.post("/create-payment", async (req, res) => {
   try {
@@ -29,6 +32,48 @@ router.post("/create-payment", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "payment creation failed" });
+  }
+});
+
+router.post("/verify-payment", async (req, res) => {
+  try {
+    const {
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature,
+      userId,
+      plan,
+    } = req.body;
+
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+      .update(body)
+      .digest("hex");
+
+    if (expectedSignature !== razorpay_signature) {
+      return res.status(400).json({ error: "Invalid payment signature" });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) return res.status(400).json({ error: "user not found" });
+
+    const expiry = new Date();
+    expiry.setDate(expiry.getDate() + 30);
+
+    user.subscription.plan = plan;
+    user.subscription.expiresAt = expiry;
+    user.subscription.applicationUsed = 0;
+
+    await user.save();
+
+    res.json({
+      message: "payment verified and subscription activated",
+      plan,
+      expiresAt: expiry,
+    });
+  } catch (error) {
+    res.status(500).json({error:"payment verification failed"})
   }
 });
 
