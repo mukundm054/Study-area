2d2e58c97cc805f0e377abf3c8114b1ea171f117
mukundm054 <mukundm054@gmail.com>
2026-01-29 16:28:29 +0530

    Update Application.js
    update application

--- Backend/Routes/Application.js
+++ Backend/Routes/Application.js
@@ -2,24 +2,48 @@ const express = require("express");
 const router = express.Router();
 
 const Application = require("../model/Application");
+const User = require("../model/User");
+const SUBSCRIPTION_PLAN = require("../config/subscriptionPlan");
 
 router.post("/", async (req, res) => {
-  const applicationData = new Application({
-    company: req.body.company,
-    category: req.body.category,
-    coverLetter: req.body.coverLetter,
-    availability: req.body.availability,
-    user: req.body.user,
-    internship: req.body.internship,
-  });
-  await applicationData
-    .save()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((error) => {
-      console.log(error);
+  try {
+    const { user, internship } = req.body;
+
+    const dbUser = await User.findById(user);
+    if (!dbUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const plan = dbUser.subscription.plan;
+    const planLimit = SUBSCRIPTION_PLAN[plan].limit;
+
+    if (dbUser.subscription.applicationUsed >= planLimit) {
+      return res
+        .status(403)
+        .json({ error: `Application Limit is reached for ${plan} plan` });
+    }
+
+    const applicationData = new Application({
+      company: req.body.company,
+      category: req.body.category,
+      coverLetter: req.body.coverLetter,
+      availability: req.body.availability,
+      user,
+      internship,
     });
+
+    await applicationData.save();
+
+    dbUser.subscription.applicationUsed += 1;
+    await dbUser.save();
+
+    res
+      .status(201)
+      .json({ message: "Application Submitted successful", applicationData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 router.get("/", async (req, res) => {
@@ -62,7 +86,7 @@ router.put("/:id", async (req, res) => {
     const updateapplication = await Application.findByIdAndUpdate(
       id,
       { $set: { status } },
-      { new: true }
+      { new: true },
     );
     if (!updateapplication) {
       res.status(404).json({ error: "Not able to update application" });
