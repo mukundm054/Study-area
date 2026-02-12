b216cb89ce64e06f9a264458f61e0a84d5549fb5
mukundm054 <mukundm054@gmail.com>
2026-02-12 15:48:21 +0530

    Update Userslice.js

--- studyarea/src/Fetaure/Userslice.js
+++ studyarea/src/Fetaure/Userslice.js
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  user:
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : null,
+};
+
 export const Userslice = createSlice({
   name: "user",
-  initialState: {
-    value: null,
-  },
+  initialState,
   reducers: {
     login: (state, action) => {
       state.user = action.payload;
+      localStorage.setItem("user", JSON.stringify(action.payload));
     },
     logout: (state) => {
       state.user = null;
+      localStorage.removeItem("user");
     },
   },
 });
